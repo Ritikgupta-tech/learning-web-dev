d895905a3fd2619af0ad86a60186cb062f295127
Ritikgupta-tech <ritik95061@gmail.com>
2026-03-20 23:56:30 +0530

    DAY-43

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,10 @@ let a=5;
 let b=10;
 console.log(a+b);
 console.log(a-b);
+let pencilPrice=10;
+let penlPrice=20;
+let output1="the price total is" + (pencilPrice+penlPrice) + "rupees";
+let output=`the total price is +${pencilPrice+penlPrice}`;
+console.log(output);
+console.log(output1);
+
