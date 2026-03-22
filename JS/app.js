64c097769da197d2452e8ae6c1c12a11a6494bb1
Ritikgupta-tech <ritik95061@gmail.com>
2026-03-22 09:04:40 +0530

    DAY-45

--- JS/app.js
+++ JS/app.js
@@ -6,8 +6,41 @@ console.log(a+b);
 console.log(a-b);
 let pencilPrice=10;
 let penlPrice=20;
-let output1="the price total is" + (pencilPrice+penlPrice) + "rupees";
-let output=`the total price is +${pencilPrice+penlPrice}`;
+let output1="the price total is " + (pencilPrice +penlPrice) + " rupees";
+let output=`the total price is ${ pencilPrice+penlPrice}`;
 console.log(output);
 console.log(output1);
+if(a!=b){
+   console.log("equal");
+}
+
+//practice question 
+let color="red";
+if (color==="red") {
+    console.log("stop");
+}
+else if (color==="yellow") {
+    console.log("slow down");
+}
+else if(color==="green") {
+    console.log("g0");
+}
+else {
+    console.log("traffic light did not activate")
+}
+// PRACTICE QUESTION
+let size="XL";
+if (size==="XL") {
+    console.log("PRICE IS RS.250");
+}
+else if (size==="L") {
+    console.log("PRICE IS RS.200");
+}
+else if(size==="M") {
+    console.log("PRICE IS RS.100");
+}
+else {
+    console.log("PRICE IS RS.50")
+}
+
 
