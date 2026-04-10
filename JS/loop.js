bae8c5424a99b8988792605145575c927a420837
Ritikgupta-tech <ritik95061@gmail.com>
2026-04-10 07:38:37 +0530

    DAY-60

--- JS/loop.js
+++ JS/loop.js
@@ -34,3 +34,48 @@ for (let i =15;i>=1;i=i-2){
     for(let j=1;j<=3;j++)
     console.log(j);
 }
+
+let i=1;
+while (i<5) {
+    if (i==3) {
+        break;
+    }
+    console.log(i);
+    i++;
+}
+/*let favmov="moneyheist";
+let guess=prompt("enter movie");
+while (favmov!=guess ) {
+    if (guess=="quit") {
+        console.log("you quit");
+        break;
+    }
+    guess=prompt("wrong ans . pls try again");
+}
+if (favmov == guess) {
+    console.log("you guess write movie");   
+}*/
+
+let fruits=["mango","apple","bannana","litchi","orange"];
+fruits.push("pineapple");
+for (let i = 0; i < fruits.length; i++) {
+    console.log(i,fruits[i]);   
+}
+for (fruit of fruits){
+    console.log("no of fruits",fruit);
+}
+
+let heroes=[["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+ for (let i = 0; i < heroes.length; i++) {
+    console.log(i,heroes[i],heroes[i].length);
+    for (let j = 0; j < heroes[i].length; j++) {
+        console.log(heroes[i][j]);
+        
+    }
+    
+ }
+ for(list of heroes){
+    for(hero of list){
+        console.log("new list of ",hero);
+    }
+ }
