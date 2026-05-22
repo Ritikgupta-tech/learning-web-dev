d843d218a927653646c0e3b1d9f16f4016327253
Ritikgupta-tech <ritik95061@gmail.com>
2026-05-22 08:10:10 +0530

    DAY-60

---
+++ object.js
@@ -0,0 +1,38 @@
+let student = {
+    Name: "ritik",
+    age: 22,
+    branch: "cse"
+};
+
+const classinfo = {
+    aman: {
+        grade: "a++",
+        city: "gkp"
+    },
+    aman1: {
+        grade: "a++",
+        city: "gkp"
+    },
+    aman3: {
+        grade: "a++",
+        city: "gkp"
+    }
+};
+
+const classin = [
+    {
+        name: "aman",
+        grade: "a++",
+        city: "gkp"
+    },
+    {
+        name: "aman1",
+        grade: "a++",
+        city: "gkp"
+    },
+    {
+        name: "aman3",
+        grade: "a++",
+        city: "gkp"
+    }
+];
