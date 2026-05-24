1b2945633558e56d8283ee78c90f7c5d293ac367
Ritikgupta-tech <ritik95061@gmail.com>
2026-05-24 15:30:55 +0530

    DAY-70

---
+++ JS/functions.js
@@ -0,0 +1,17 @@
+function rolldice(){
+    let rand = Math.floor(Math.random()*6)+1;
+    console.log(rand);
+}
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
+rolldice();
