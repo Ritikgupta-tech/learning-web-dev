7aacdfc46d9ba7383a4e7b84ee999bd9375b50a0
Ritikgupta-tech <ritik95061@gmail.com>
2026-06-08 23:09:02 +0530

    DAY-88

---
+++ backend/math.js
@@ -0,0 +1,14 @@
+const sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const g=9.18;
+const pi=3.14;
+
+let obj = {
+    sum:sum,
+    mul:mul,
+    g: g,
+    pi: pi
+};
+module.exports=123;
+module.exports=obj;
+
---
+++ backend/script.js
@@ -0,0 +1,13 @@
+/*let args = process.argv;
+for(let i=0;i<args.length;i++){
+    console.log("heelo to ",args[i]);
+}
+
+
+//const fruits = require("./fruits");
+const someValue=require("./math");
+console.log(someValue);
+console.log(someValue.pi);*/
+
+const info = require("./Fruits");
+console.log(info);
